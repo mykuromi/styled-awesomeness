352aac06e61b77575e3dc850d7045c447b8378ca
Heera <92657209+mykuromi@users.noreply.github.com>
2023-09-29 19:17:38 +0900

    1. set-up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="button button-success">Hello</button>
+      <button className="button button--danger">Hello</button>
+    </>
+  );
+}
+export default App;
