b1590e8cd7196c623d88db75162cd48a09aafbe1
Heera <92657209+mykuromi@users.noreply.github.com>
2023-09-30 12:31:00 +0900

    3. createGlobalStyle and extending styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <Container>
-      <Button>Hello</Button>
-      <Button danger>Hello</Button>
-    </Container>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Button>Hello</Button>
+        <Button danger>Hello</Button>
+        <Anchor as={Button} href="http://google.com">
+          Go to google
+        </Anchor>
+        <Anchor2 href="http://google.com">Go to google</Anchor2>
+      </Container>
+    </>
   );
 }
 
@@ -28,6 +42,14 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Anchor = styled.a`
+  text-decoration: none;
+`;
+
+const Anchor2 = styled(Button)`
+  text-decoration: none;
 `;
 
 export default App;
